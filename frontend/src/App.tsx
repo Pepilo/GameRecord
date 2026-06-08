77c4ba7ea751a41808c2d272493ae30291aa4e7f
Lopepi <lopepi@outlook.fr>
2026-06-08 21:27:05 +0200

    Testing components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,31 @@ import './App.css'
 import Home from './components/Home'
 import ListGroup from './components/ListGroup'
 
+const backlog = [
+  "Red dead redemption 2",
+  "The witcher 3",
+  "Clair obscur Expidition 33",
+  "Bloodborne",
+  "Monster hunter world"
+]
+
+const wishlist = [
+  "Elden Ring",
+  "Persona 5",
+  "Hollow Knight"
+]
+
+const handleSelectGame = (game: string) => {
+  console.log(game);
+}
+
 function App() {
-  return <div><ListGroup/></div>
+  return (
+    <>
+      <div><ListGroup games={backlog} heading="Backlog" onSelectGame={handleSelectGame}/></div>
+      <div><ListGroup games={wishlist} heading="Wishlist" onSelectGame={handleSelectGame}/></div>
+    </>
+  );
 }
 
 export default App
--- frontend/src/components/ListGroup.tsx
+++ frontend/src/components/ListGroup.tsx
@@ -1,27 +1,24 @@
-function ListGroup() {
+import { useState, type MouseEvent} from "react";
 
-    const games = [
-        "Red dead redemption 2",
-        "The witcher 3",
-        "Clair obscur Expidition 33",
-        "Bloodborne",
-        "Monster hunter world"
-    ]
+interface Props {
+    games: string[];
+    heading: string;
+    onSelectGame: (game: string) => void;
+}
 
-    if (games.length === 0)
-        return (
-            <>
-                <h1>Game list</h1>
-                <p>No game found.</p>
-            </>
-        );
+function ListGroup({games, heading, onSelectGame}: Props) {
+
+    const handleClick = (event: MouseEvent) => console.log(event);
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return ( 
         <>
-            <h1>Game list</h1>
+            <h1>{heading}</h1>
+            {games.length === 0 && <p>No game found.</p>}
             <ul className="list-group">
-                {games.map((game) =>(<li key = {game}>{game}</li>))}
-            </ul>;
+                {games.map((game, index) => (<li className= {selectedIndex === index ? "list-group-item active" : "list-group-item"} key = {game} onClick={() => {setSelectedIndex(index); onSelectGame(game)} }>{game}</li>))}
+            </ul>
         </>
     );
 }
