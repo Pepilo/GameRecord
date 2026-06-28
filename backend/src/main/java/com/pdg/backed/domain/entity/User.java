c33a2cef0468718e5e0080f5c42c3711710b0e57
Lopepi <lopepi@outlook.fr>
2026-06-28 16:08:44 +0200

    Create user-review relation

--- backend/src/main/java/com/pdg/backed/domain/entity/User.java
+++ backend/src/main/java/com/pdg/backed/domain/entity/User.java
@@ -1,17 +1,21 @@
 package com.pdg.backed.domain.entity;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,6 +34,9 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String userName;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
 
     @CreationTimestamp
     @Column(nullable = false)
