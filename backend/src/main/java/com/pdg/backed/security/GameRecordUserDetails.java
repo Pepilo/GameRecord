d621ba601091a807281461c7cfc8f633d2e31946
Lopepi <lopepi@outlook.fr>
2026-06-30 11:19:27 +0200

    Update to user details

--- backend/src/main/java/com/pdg/backed/security/GameRecordUserDetails.java
+++ backend/src/main/java/com/pdg/backed/security/GameRecordUserDetails.java
@@ -3,21 +3,24 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.pdg.backed.domain.entity.User;
 
-public class CustomUserPrincipal implements UserDetails {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class GameRecordUserDetails implements UserDetails {
 
     private final User user;
 
-    public CustomUserPrincipal(User user) {
-        this.user = user;
-    }
-
     @Override
     public String getUsername() {
-        return user.getUserName();
+        return user.getEmail();
     }
 
     @Override
@@ -27,7 +30,7 @@ public class CustomUserPrincipal implements UserDetails {
 
    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
@@ -54,7 +57,7 @@ public class CustomUserPrincipal implements UserDetails {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        CustomUserPrincipal that = (CustomUserPrincipal) o;
+        GameRecordUserDetails that = (GameRecordUserDetails) o;
         return user.getUserId().equals(that.user.getUserId());
     }
 }
