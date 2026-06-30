0b8b08f4a843293c2f61732465aad1f6a2361d11
Lopepi <lopepi@outlook.fr>
2026-06-30 11:11:02 +0200

    Update to security config

--- backend/src/main/java/com/pdg/backed/config/SecurityConfig.java
+++ backend/src/main/java/com/pdg/backed/config/SecurityConfig.java
@@ -2,51 +2,51 @@ package com.pdg.backed.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomUserDetailsService customUserDetailsService;
-    private final JwtUtils jwtUtils;
-    private final JwtFilter jwtFilter;
+    // private final CustomUserDetailsService customUserDetailsService;
+    // private final JwtUtils jwtUtils;
+    // private final JwtFilter jwtFilter;
 
-    public SecurityConfig (CustomUserDetailsService customUserDetailsService, JwtUtils jwtUtils, JwtFilter jwtFilter) {
-        this. customUserDetailsService = customUserDetailsService;
-        this.jwtUtils = jwtUtils;
-        this.jwtFilter = jwtFilter;
-    }
+    // public SecurityConfig (CustomUserDetailsService customUserDetailsService, JwtUtils jwtUtils, JwtFilter jwtFilter) {
+    //     this. customUserDetailsService = customUserDetailsService;
+    //     this.jwtUtils = jwtUtils;
+    //     this.jwtFilter = jwtFilter;
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);
-        return authenticationManagerBuilder.build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        return http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth ->
-                auth.requestMatchers("/api/v1/auth/*").permitAll()
-                    .anyRequest().authenticated()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/*").permitAll()
+                .anyRequest().authenticated()
             )
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+            // .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        return http.build();
     }
 }
